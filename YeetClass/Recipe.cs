d7a956d881302cb6a615c0cc7a712dc5b35f7031
ebognol@gmail.com <ebognol@gmail.com>
2022-02-18 17:32:19 +0100

    set up project

---
+++ YeetClass/Recipe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YeetClass
+{
+    public class Recipe
+    {
+        private String name;
+        private String description;
+        private String imagePath;
+        private int duration;
+        private int score;
+        private List<String> ingredients = new List<String>();
+        private List<String> instructions = new List<String>();
+
+        public Recipe(string name, string description, string imagePath, int duration, int score, List<string> ingredients, List<string> instructions)
+        {
+            this.description = description;
+            this.imagePath = imagePath;
+            this.duration = duration;
+            this.score = score;
+            this.ingredients = ingredients;
+            this.instructions = instructions;
+        }
+    }
+}
